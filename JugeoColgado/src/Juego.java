14802ac06f9bf0c83422598b85bc6335eaf4da6d
aaartur23 <artur.solsona.2018@gmail.com>
2024-11-19 16:55:07 +0100

    JuegoUpdate5
    Añado el mostrar la palabra mediante guiones y unas variables que necesitare `para el juego. Cambio los comentarios a primera persona para ser mas claro.

--- JugeoColgado/src/Juego.java
+++ JugeoColgado/src/Juego.java
@@ -11,6 +11,8 @@ public class Juego {
 		int option = 0;
 		String palabraSecreta = "";
 		String[] jugadores;
+		String palabraGuiones = "";
+		int intentosMax = 6; 
 		
 		System.out.println("*-------------------------------*");
 		System.out.println("|BIENVENIDO AL JUEGO DEL COLGADO|");
@@ -19,16 +21,16 @@ public class Juego {
 		do {
 		System.out.println("Vas a querer jugar? 1=si 2=no");
 		option = sc.nextInt();
-		sc.nextLine(); // Sacar el salto de linea por error
+		sc.nextLine(); // Saco el salto de linea por error
 		
 		if(option == 1) {
 			System.out.println("*EMPECEMOS*");
 		
-		// Pedir al usuario que ingrese la palabra
+		// Pido al usuario que ingrese la palabra
         System.out.print("Por favor, ingresa la palabra para adivinar: ");
         palabraSecreta = sc.nextLine().toLowerCase(); // Convertir la palabra a minúsculas para evitar problemas con mayúsculas
         
-        // Preguntar cuántos jugadores van a jugar
+        // Pregunto cuántos jugadores van a jugar
         System.out.print("¿Cuántos jugadores van a jugar? ");
         numeroDeJugadores = sc.nextInt();  // Leer el número de jugadores
         
@@ -38,22 +40,32 @@ public class Juego {
         } else {
         	
         
-        // Crear un array con el número de jugadores
+        // Creo un array con el número de jugadores
         jugadores = new String[numeroDeJugadores];
         
-        // Pedir el nombre de cada jugador
+        // Pido el nombre de cada jugador
         for (int i = 0; i < jugadores.length; i++) {
             System.out.print("Ingresa el nombre del jugador " + (i + 1) + ": ");
             jugadores[i] = sc.next();  // Leer el nombre del jugador y almacenarlo en el array
            }
-        // Mostrar la palabra ingresada
+        
+        // Inicializo la palabra oculta con guiones
+        palabraGuiones = "_ ".repeat(palabraSecreta.length()); // muestro tantos guiones como letras de la palabra
+        System.out.println("La palabra secreta tiene " + palabraSecreta.length() + " letras.");
+        System.out.println("La palabra oculta es: " + palabraGuiones);
+        
+        // Variables para el juego
+        int intentosRestantes = intentosMax;
+        String palabraActual = palabraGuiones;
+        boolean palabraAdivinada = false;
+        
+        // Muestro la palabra ingresada
         System.out.println("La palabra secreta es: " + palabraSecreta);
         }
 		} else {
 			System.out.println("Has salido correctamente");
 		}
 		} while (option != 2);
-        
         
 		
 	}
